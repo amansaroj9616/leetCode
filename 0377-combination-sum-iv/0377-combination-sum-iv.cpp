73e10f201e0302ff57e8613d20c81182f9bf801b
Aman Saroj <134101401+amansaroj9616@users.noreply.github.com>
2025-10-08 23:25:09 +0530

    Time: 0 ms (100%), Space: 9.3 MB (21.34%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        int n = nums.size();
         vector<unsigned long long> dp(target + 1, 0); 
         dp[0] = 1;  
         
